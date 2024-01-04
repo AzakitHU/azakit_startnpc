5082c587d8d3a08a9826b242fc9d0c5b11f7fa9f
AzakitHU <155274165+AzakitHU@users.noreply.github.com>
2024-01-04 18:10:25 +0100

    Update main.lua
    Create Discord Webhook

--- server/main.lua
+++ server/main.lua
@@ -7,6 +7,8 @@ RegisterServerCallback('azakit_startnpc:itemTaken',function(source, cb)
         xPlayer.removeInventoryItem(ITEM, ITEM_AMOUNT)  -- item, amount
        -- xPlayer.removeInventoryItem(ITEM2, ITEM_AMOUNT2)  -- item, amount
         cb(true)
+        local message = "**Steam:** " .. GetPlayerName(source) .. "\n**Identifier:** " .. xPlayer.identifier .. "\n**ID:** " .. source .. "\n**Log:** Start " ..EVENT.."."
+                discordLog(message, Webhook)
         else
         cb(false)
         end
@@ -15,6 +17,8 @@ RegisterServerCallback('azakit_startnpc:itemTaken',function(source, cb)
         --xPlayer.removeInventoryItem(ITEM, ITEM_AMOUNT)  -- item, amount
        -- xPlayer.removeInventoryItem(ITEM2, ITEM_AMOUNT2)  -- item, amount
         cb(true)
+        local message = "**Steam:** " .. GetPlayerName(source) .. "\n**Identifier:** " .. xPlayer.identifier .. "\n**ID:** " .. source .. "\n**Log:** Start " ..EVENT.."."
+                discordLog(message, Webhook)
         else
         cb(false)
         end
@@ -31,6 +35,8 @@ RegisterServerCallback('azakit_startnpc:itemTaken2',function(source, cb)
         xPlayer.removeInventoryItem(ITEM2, ITEM_AMOUNT2)
         -- xPlayer.removeInventoryItem(ITEM3, ITEM_AMOUNT3)  -- item, amount
         cb(true)
+        local message = "**Steam:** " .. GetPlayerName(source) .. "\n**Identifier:** " .. xPlayer.identifier .. "\n**ID:** " .. source .. "\n**Log:** Start " ..EVENT2.."."
+                discordLog(message, Webhook)
         else
         cb(false)
         end
@@ -39,6 +45,8 @@ RegisterServerCallback('azakit_startnpc:itemTaken2',function(source, cb)
            -- xPlayer.removeInventoryItem(ITEM2, ITEM_AMOUNT2)
             -- xPlayer.removeInventoryItem(ITEM3, ITEM_AMOUNT3)  -- item, amount
             cb(true)
+            local message = "**Steam:** " .. GetPlayerName(source) .. "\n**Identifier:** " .. xPlayer.identifier .. "\n**ID:** " .. source .. "\n**Log:** Start " ..EVENT2.."."
+                    discordLog(message, Webhook)
             else
             cb(false)
         end
@@ -54,6 +62,8 @@ RegisterServerCallback('azakit_startnpc:itemTaken3',function(source, cb)
         xPlayer.removeInventoryItem(ITEM3, ITEM_AMOUNT3)
         -- xPlayer.removeInventoryItem(ITEM2, ITEM_AMOUNT2)  -- item, amount
         cb(true)
+        local message = "**Steam:** " .. GetPlayerName(source) .. "\n**Identifier:** " .. xPlayer.identifier .. "\n**ID:** " .. source .. "\n**Log:** Start " ..EVENT3.."."
+                discordLog(message, Webhook)
          else
         cb(false)
          end
@@ -62,8 +72,15 @@ RegisterServerCallback('azakit_startnpc:itemTaken3',function(source, cb)
        -- xPlayer.removeInventoryItem(ITEM3, ITEM_AMOUNT3)
         -- xPlayer.removeInventoryItem(ITEM2, ITEM_AMOUNT2)  -- item, amount
         cb(true)
+        local message = "**Steam:** " .. GetPlayerName(source) .. "\n**Identifier:** " .. xPlayer.identifier .. "\n**ID:** " .. source .. "\n**Log:** Start " ..EVENT3.."."
+                discordLog(message, Webhook)
          else
         cb(false)
          end
     end        
 end)
+
+
+function discordLog(message, webhook)
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = 'AzakitBOT', embeds = {{["description"] = "".. message .."",["footer"] = {["text"] = "Azakit Development - https://discord.com/invite/DmsF6DbCJ9",["icon_url"] = "https://cdn.discordapp.com/attachments/1150477954430816456/1192512440215277688/azakitdevelopmentlogoavatar.png?ex=65a958c1&is=6596e3c1&hm=fc6638bef39209397047b55d8afbec6e8a5d4ca932d8b49aec74cb342e2910dc&",},}}, avatar_url = "https://cdn.discordapp.com/attachments/1150477954430816456/1192512440215277688/azakitdevelopmentlogoavatar.png?ex=65a958c1&is=6596e3c1&hm=fc6638bef39209397047b55d8afbec6e8a5d4ca932d8b49aec74cb342e2910dc&"}), { ['Content-Type'] = 'application/json' })
+end
